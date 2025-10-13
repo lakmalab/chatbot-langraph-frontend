b6884ee3841fc10870a42a096fee450230ecf154
lakma <lakmalaberathne@gmail.com>
2025-10-13 11:08:21 +0530

    feat - added pension page and tailweind css

---
+++ chatbot-langraph-frontend/src/pages/PensionPage.tsx
@@ -0,0 +1,13 @@
+
+
+function PensionPage() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-center text-3xl font-bold ">Hi Lakmal</h1>
+    </>
+  )
+}
+
+export default PensionPage
